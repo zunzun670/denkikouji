19959293bd7df43ba0e22e742e731bb946851a5c
zunzun670 <kazho4zl@gmail.com>
2026-02-08 19:01:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 import streamlit as st
 
-question = "一般住宅の屋内配線で「VVFケーブル」を使用する目的として最も適切なのはどれ？" 
-choices = [ 
+questions = [
+   {
+              "q":"一般住宅の屋内配線で「VVFケーブル」を使用する目的として最も適切なのはどれ？" 
+"choices" = [ 
            "1: 高電圧を長距離送電するため", 
            "2: 屋内の固定配線に使うため", 
            "3: 地中に直接埋設するため", 
-           "4: 防爆エリアで使用するため" ]
+           "4: 防爆エリアで使用するため" ],
 correct = "2" 
+"info":"VVFケーブルは住宅などの屋内の固定配線に使われるケーブルだね！絶縁体と被覆が一体化していて施工しやすいのが特徴だよ。"
+    }
+]
+
+if "index" not in st.session_state:
+           st.session_state.index = 0
+
+current = questions[st.session_state.index] 
 
 st.write("### 問題") 
-st.write(question) 
+st.write(current["q"])
 
-selected = st.radio("選択肢を選んでね", choices, index=None)
+selected = st.radio("選択肢を選んでね", current["choices"], index=None)
 
 if selected:
-    if selected.startswith(correct):
+    if selected.startswith(current["correct"]):
         st.success("正解！☺")
-        st.info("VVFケーブルは住宅などの屋内の固定配線に使われるケーブルだね！絶縁体と被覆が一体化していて施工しやすいのが特徴だよ。") 
+        st.info(current["info"])
     else: st.error("ざんねん😭")
+
+if st.button("次へ"): 
+    st.session_state.index += 1
+    if st.session_state.index >= len(questions): 
+               st.session_state.index = 0 # 最後まで行ったら最初に戻る
+    st.experimental_rerun()
