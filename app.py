0fb6654aef580fc316d41294e58e963c8d80291d
zunzun670 <kazho4zl@gmail.com>
2026-02-08 18:08:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.write(question)
 selected_key = st.radio("選択肢を選んでね", list(choices.keys()))
 
 if selected_key is not None: 
-if selected_key == "2":
-   st.success("正解！☺") 
-   st.info("VVFケーブルは住宅などの屋内の固定配線に使われるケーブルだね！絶縁体と被覆が一体化していて施工しやすいのが特徴だよ。") 
- else: st.error("ざんねん😭")
+    if selected_key == "2"
+           st.success("正解！☺") 
+           st.info("VVFケーブルは住宅などの屋内の固定配線に使われるケーブルだね！絶縁体と被覆が一体化していて施工しやすいのが特徴だよ。") 
+    else: st.error("ざんねん😭")
