db280471950d6fc6b015bf7c1842e865880950c4
zunzun670 <kazho4zl@gmail.com>
2026-02-08 18:18:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ correct = "2"
 st.write("### 問題") 
 st.write(question) 
 
-selected = st.radio("選択肢を選んでね", choices)
+selected = st.radio("選択肢を選んでね", choices, index=None)
 
-if selected is not None:
+if selected:
     if selected.startswith(correct):
         st.success("正解！☺")
         st.info("VVFケーブルは住宅などの屋内の固定配線に使われるケーブルだね！絶縁体と被覆が一体化していて施工しやすいのが特徴だよ。") 
